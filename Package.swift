b0a27becdea9fe57011e78e42089cb027144f50e
Eric Hyche <erich@noom.com>
2025-10-17 15:44:53 -0400

    Create library target

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "pjlink-client-cli",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .target(name: "pjlink-client-lib"),
             ]
         ),
+        .target(name: "pjlink-client-lib"),
     ]
 )
